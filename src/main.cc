26ff32c64c41130757d6c89e708e4f27b00a911c
astridcastrox <castro.astrid.cbtis37@gmail.com>
2021-08-24 20:28:17 -0700

    first commit

---
+++ src/main.cc
@@ -0,0 +1,11 @@
+#include<iostream>
+#include "MyHeader.hh"
+
+int main()
+{
+  std::cout << "Suma: " << Add(1, 2) << std::endl;
+  std::cout << "Suma: " << Add(1.f, 2.5f) << std::endl;
+  std::cout << "Suma: " << Add("Hello ", "World!") << std::endl;
+  std::cin.get();
+  return 0;
+}
